72c7eb3e836a9dd95e2f56ca2688cec07c89dcbe
TinTDang <tin2dang@gmail.com>
2018-09-22 21:26:18 -0500

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import Navbar from "./components/Navbar"
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Temp</h1>
+      <div>
+        <Navbar />
+        
       </div>
     );
   }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css"
+
+const Navbar = () => 
+<nav>
+  <div className="nav-extended">
+  <div className="nav-content">
+  <h1 className="nav-title center">New York Times Article Scrubber</h1>
+  </div>
+  </div>
+</nav>;
+
+export default Navbar;
