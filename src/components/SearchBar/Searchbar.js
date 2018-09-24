1a1c60d12786a112a844914a4622fe0d8aabb89d
TDang <tin2dang@gmail.com>
2018-09-23 19:07:31 -0500

    added card stylings to searchbar.js

--- src/components/SearchBar/Searchbar.js
+++ src/components/SearchBar/Searchbar.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./SearchBar.css"
 
 const SearchBar = () => 
-<div>
+<div className="card">
+    <div className="card-header">
+        SearchBar
+    </div>
+    <div className="container">
+        <div className="card-body">
+
+        </div>
+    </div>
 </div>;
 
 export default SearchBar;
