5be84a9e9cf93a76b9218d3f09038f57958e0a66
TinTDang <tin2dang@gmail.com>
2018-09-24 19:42:37 -0500

    changed app.js standard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Navbar from "./components/Navbar"
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        
-      </div>
-    );
-  }
-}
+
+const App = () =>
+  <Router>
+    <div>
+      <Navbar />
+
+    </div>
+  </Router>;
+
 
 export default App;
